d84c9307de5462f742388aa5bbfcb779238b2dbb
Kartik Patel <Paresh@Pareshs-Mac-mini.local>
2016-12-17 12:50:22 +0530

    Initialize app
    Initialize KTContacts Swift app in Xcode

---
+++ KTContacts/ContactsOverviewController.swift
@@ -0,0 +1,25 @@
+//
+//  ContactsOverviewController.swift
+//  KTContacts
+//
+//  Created by Kartik Patel on 12/17/16.
+//  Copyright © 2016 Kartik Patel. All rights reserved.
+//
+
+import UIKit
+
+class ContactsOverviewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
